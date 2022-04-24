f2ba09fc2758ac9469a15fa953d3aaed3d5215d7
DanylDolgii <danyldolgii777@gmail.com>
2022-04-25 00:59:16 +0300

    Update view class: add input and output

--- src/views/PurchaseView.java
+++ src/views/PurchaseView.java
@@ -1,6 +1,7 @@
 package views;
 
 import models.Purchase;
+import utils.Validator;
 
 import java.util.Scanner;
 
@@ -17,5 +18,37 @@ public class PurchaseView {
         // Конструктор
         public PurchaseView(Purchase model) {
             this.model = model;
+        }
+
+        // Ввод данных
+        public void GetInputs() {
+
+                // Создаем экземпляр Scanner
+                scanner = new Scanner (System.in);
+
+                // Ввод и валидация данных
+
+                title = "Client name: ";
+                System.out.print(title);
+                name = Validator.validateName(scanner);
+                model.setClientName(name);
+
+                title = "Quantity of goods (pieces): ";
+                System.out.print(title);
+                quantity = Validator.validateQuantityInput(scanner);
+                model.setQuantity(quantity);
+
+                title = "Price of the goods piece (USD): ";
+                System.out.print(title);
+                price = Validator.validatePriceInput(scanner);
+                model.setPrice(price);
+
+                // Закрываем Scanner
+                scanner.close();
+        }
+
+        // Вывод данных
+        public void getOutput(String output) {
+                System.out.println(output);
         }
 }
