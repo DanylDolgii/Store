517f2744009f57c7cf2b81dd6bdacdb640e852cf
DanylDolgii <danyldolgii777@gmail.com>
2022-04-24 23:45:25 +0300

    Add controller class

---
+++ src/controllers/PurchaseController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import models.Purchase;
+import views.PurchaseView;
+
+public class PurchaseController {
+    Purchase model;
+    PurchaseView view;
+
+    // Конструктор
+    public PurchaseController(Purchase model, PurchaseView view) {
+        this.model = model;
+        this.view = view;
+    }
+}
